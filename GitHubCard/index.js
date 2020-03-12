3df942ac7c5dc890ce8ea80d2afa7921ad3c21d5
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-03-12 13:18:09 -0700

    finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/michaelbarnes7282')
+.then( response => {
+  console.log(response.data);
+  cards.append(createCard(response));
 
+})
+.catch(error => {
+  console.log('the data was not returned', error);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -13,6 +21,7 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+const cards = document.querySelector('.cards');
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -24,7 +33,25 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "cladams0203",
+  "Ezera-Black",
+  "christianlewis024",
+  "mpaolodr",
+  "easpaas"
+];
+
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+.then( response => {
+  console.log(response.data);
+  cards.append(createCard(response));
+
+})
+.catch(error => {
+  console.log('the data was not returned', error);
+})
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -34,8 +61,7 @@ const followersArray = [];
   <div class="card-info">
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Location: {users location}</p>      
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -45,6 +71,44 @@ const followersArray = [];
 </div>
 
 */
+function createCard(hub){
+  // Variable declarations
+  const card = document.createElement('div'),
+        userImg = document.createElement('img'),
+        info = document.createElement('div'),
+        name = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        link = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+  // Adding classes
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  // Appending to parents
+  card.append(userImg, info);
+  info.append(name, userName, location, followers, following, bio);
+  location.append(link);
+
+  // Assigning Values
+  userImg.src = hub.data.avatar_url;
+  name.textContent = hub.data.name;
+  userName.textContent = hub.data.login;
+  location.textContent = hub.data.location;
+  link.textContent = hub.data.url;
+  followers.textContent = hub.data.followers;
+  following.textContent = hub.data.following;
+  bio.textContent = hub.data.bio;
+  
+
+  return card;
+}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
